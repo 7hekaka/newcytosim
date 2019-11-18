665331ea2c9291ad31e5fd8ec653724b2152b298
nedelec <nedelec@embl.de>
2019-11-18 09:46:57 +0000

    insignificant rewrite to uniformize Vector2

--- src/math/matrix34.h
+++ src/math/matrix34.h
@@ -957,7 +957,6 @@ public:
     /// return a random rotation that transforms (1,0,0) into `vec` ( norm(vec) should be > 0 )
     /**
      In 3D, this rotation is chosen uniformly among all the rotation transforming (1,0,0) into `vec`.
-     The function will fail if ( vec == 0 ).
      */
     static Matrix34 randomRotationToVector(const Vector3&);
     
