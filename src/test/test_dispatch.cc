aa9c98dfd1f7edb912967d22416d8a2b48c81b2b
nedelec <nedelec@embl.de>
2019-11-18 08:39:07 +0000

    better test_dispatch

--- src/test/test_dispatch.cc
+++ src/test/test_dispatch.cc
@@ -1,14 +1,31 @@
 #include <dispatch/dispatch.h>
 #include <stdio.h>
+#include <math.h>
 
-int main() {
-  dispatch_queue_t queue = dispatch_queue_create(NULL, NULL); 
 
-  dispatch_sync(queue, ^{
-    printf("Hello, world from a dispatch queue!\n");
-  });
+const size_t CNT = 1024;
 
-  dispatch_release(queue);
+double results[CNT];
 
-  return 0;
+void work(size_t i)
+{
+    results[i] = sin(i);
 }
+
+
+int main(int argc, char * argv[])
+{
+    //dispatch_queue_t queue = dispatch_queue_create(nullptr, nullptr)
+    dispatch_queue_t const& queue = dispatch_get_global_queue(0, 0);
+    
+    dispatch_apply(CNT, queue, ^(size_t i){ work(i); });
+    
+    dispatch_sync(queue, ^{ printf("Hello, from a dispatch queue!\n"); });
+
+    //dispatch_release(queue);
+    for ( int i = 0; i < 10; ++i )
+        printf("%i %9.3f\n", i, results[i]);
+    
+    return 0;
+}
+
