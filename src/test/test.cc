e5e9c8652e546ebfac6d5b0983e1fab36f3feb95
francois.nedelec@slcu.cam.ac.uk <francois.nedelec@slcu.cam.ac.uk>
2020-09-23 17:28:06 +0200

    reversed code

--- src/test/test.cc
+++ src/test/test.cc
@@ -16,21 +16,20 @@ template < typename T >
 std::string to_bits(const T& val, char spc = 0)
 {
     unsigned char * ptr = (unsigned char*) & val;
-    char res[1+(1+CHAR_BIT)*sizeof(T)];
-    char * out = res;
+    char res[1+sizeof(T)*(CHAR_BIT+1)] = {0};
+    char * out = res + CHAR_BIT * sizeof(T) + (spc?sizeof(T):0) - 1;
     
-    for ( int i = sizeof(T)-1; i >= 0; --i)
+    for ( size_t i = 0; i < sizeof(T); ++i )
     {
         unsigned char byte = ptr[i];
-        for ( int i = 0; i < CHAR_BIT; ++i )
+        for ( size_t j = 0; j < CHAR_BIT; ++j )
         {
-            *out++ = '0' + ( 1 & ( byte >> (CHAR_BIT-1) ));
-            byte <<= 1;
+            *out-- = '0' + ( 1 & byte );
+            byte >>= 1;
         }
         if ( spc )
-            *out++ = spc;
+            *out-- = spc;
     }
-    *out = 0;
     return std::string(res);
 }
 
