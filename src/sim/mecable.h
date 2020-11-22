7hekaka/newcytosim

commit 3c2809a5ab739a39c2b370bd787527d5b2df8a60

SIZE_T offers new possibility to reduce memory usage

src/sim/mecable.h
@@ -1,4 +1,4 @@
-// Cytosim was created by Francois Nedelec. Copyright 2007-2017 EMBL.
+// Cytosim was created by Francois Nedelec. Copyright 2020 Cambridge University
 
 
 #ifndef MECABLE_H
@@ -16,6 +16,11 @@ class Meca;
 #define EXPERIMENTAL_PRECONDITIONNERS 0
 
 
+/// to save memory, SIZE_T could be defined to use down to 2 bytes.
+/* The limit imposed on the size of the Mecable is hardly relevant anyhow */
+typedef unsigned short SIZE_T;
+
+
 /// Can be simulated using a Meca.
 /**
  A Mecable is an Object made of points that can can be simulated in a Meca.
@@ -24,8 +29,9 @@ class Meca;
  It derives from Movable, and thus can be translated or rotated.
  Mecable is also a Buddy, and can thus be part of an Organizer.
  
- This is one of the fundamental class of Cytosim.
- Merged PointSet on 20/01/2018
+ This is one of the fundamental class of Cytosim, working closely with Meca.
+ 
+ Acquired PointSet on 20/01/2018.
  */
 class Mecable : public Object, public Buddy
 {
@@ -44,15 +50,18 @@ protected:
     real const* pForce;
     
     /// Number of points in pForce[]
-    size_t      pForceMax;
+    SIZE_T      pForceMax;
     
     /// Number of points in the Mecable
-    size_t      nPoints;
+    SIZE_T      nPoints;
 
 private:
+    
+    /// Index that Object coordinates occupy in the matrices and vectors of Meca
+    SIZE_T      pIndex;
 
     /// Currently allocated size of arrays pPos[]
-    size_t      pAllocated;
+    SIZE_T      pAllocated;
 
     /// Matrix block used for preconditionning in Meca::solve()
     real *      pBlock;
@@ -61,13 +70,13 @@ private:
     int *       pPivot;
     
     /// Allocated size of pBlock[]
-    size_t      pBlockAlc;
+    SIZE_T      pBlockAlc;
     
     /// Allocated size of pPivot[]
-    size_t      pPivotAlc;
+    SIZE_T      pPivotAlc;
     
     /// Current size of pBlock[]
-    size_t      pBlockSize;
+    SIZE_T      pBlockSize;
     
     /// type of block
     int         pBlockType;
@@ -79,9 +88,6 @@ private:
     /// Matrix used for preconditionning
     MatrixFull  pBlockMatrix;
 #endif
-    
-    /// Index that Object coordinates occupy in the matrices and vectors of Meca
-    size_t      pIndex;
 
     /// Clear pointers
     void        clearMecable();

src/base/buddy.h
@@ -70,6 +70,7 @@ private:
             *i = b;
         else
             buddies_.push_back(b);
+        //std::clog << this << " has " << buddies_.size() << " buddies\n";
     }
     
     /// removes `b` from the list of known buddy, do not call goodbye()

src/sim/chain.cc
@@ -1892,7 +1892,7 @@ Interpolation Chain::interpolateM(const real ab) const
 {
     real a = std::max(ab, (real)0) / fnCut;
     //beyond the last point, we interpolate the PLUS_END
-    size_t s = std::min((size_t)a, nPoints-2);
+    size_t s = std::min((size_t)a, (size_t)nPoints-2);
     return Interpolation(this, s, s+1, std::min(a-s, (real)1));
 }
 

src/sim/common.h
@@ -36,8 +36,8 @@ enum AssemblyState
 };
 
 
-// used as function argument to define an AssemblyState
-/* This is needed as ENUM are treated as signed int */
+/// used as function argument to define an AssemblyState
+/** This is needed as ENUM are treated by default as signed int */
 typedef unsigned state_t;
 
 

src/sim/fibers/dynamic_fiber.cc
@@ -70,7 +70,7 @@ void DynamicFiber::setEndStateM(state_t s)
     
     if ( s != mStateM )
     {
-        mStateM  = s;
+        mStateM = s;
         unitM[1] = ( 4 - s ) / 2;
         unitM[0] = ( 4 - s - 2*unitM[1] );
         assert_true( 0==unitP[0] || unitP[0]==1 );
@@ -80,6 +80,9 @@ void DynamicFiber::setEndStateM(state_t s)
 }
 
 
+/**
+ Minus end can shrink
+ */
 int DynamicFiber::stepMinusEnd()
 {
     int res = 0;
@@ -139,7 +142,7 @@ void DynamicFiber::setEndStateP(state_t s)
     
     if ( s != mStateP )
     {
-        mStateP   = s;
+        mStateP = s;
         unitP[1] = ( 4 - s ) / 2;
         unitP[0] = ( 4 - s - 2*unitP[1] );
         assert_true( 0==unitP[0] || unitP[0]==1 );

src/sim/fibers/dynamic_fiber.h
@@ -13,68 +13,63 @@ class DynamicFiberProp;
 
 /// A Fiber with discrete growth and dynamic instability at the PLUS_END
 /**
-
  This implements the microtubule dynamic instability model proposed by
  Brun, Rupp et al. with a 'hard-coded' coupling parameter N=2.
  
  Assembly and disassembly follow discrete steps of size `prop->unit_length`.
- The model keeps track of the discrete state of the two terminal units of tubulin.
- This leads to 4 different states, which are mapped to [STATE_GREEN, STATE_RED].
+ The model keeps track of the state of the two terminal units (ie. tubulin heterodimers).
+ This leads to 4 different states, which are mapped to states [GREEN YELLOW ORANGE RED].
  
  
- The growth speed is reduced under antagonistic force by an exponential factor:\n
- <em>
- <b>Measurement of the Force-Velocity Relation for Growing Microtubules</b>\n
- Marileen Dogterom and Bernard Yurke\n
- Science Vol 278 pp 856-860; 1997\n
- http://www.sciencemag.org/content/278/5339/856.abstract
- </em>
+ The growth speed is reduced under antagonistic force by an exponential factor:
  
- ...and this increases the catastrophe rate:\n
- <em>
- <b>Dynamic instability of MTs is regulated by force</b>\n
- M.Janson, M. de Dood, M. Dogterom.\n
- Journal of Cell Biology Vol 161, Nb 6, 2003\n
- Figure 2 C\n
- </em>
- http://www.jcb.org/cgi/doi/10.1083/jcb.200301147
+***Measurement of the Force-Velocity Relation for Growing Microtubules***
+Marileen Dogterom and Bernard Yurke
+Science Vol 278 pp 856-860; 1997
+http://www.sciencemag.org/content/278/5339/856.abstract
+ 
+...and this will increase the catastrophe rate:
+ 
+***Dynamic instability of MTs is regulated by force***
+M.Janson, M. de Dood, M. Dogterom.
+Journal of Cell Biology Vol 161, Nb 6, 2003
+Figure 2 C
+http://www.jcb.org/cgi/doi/10.1083/jcb.200301147
  
  
- If you use this model, please cite:\n
- <em>
- <b>A theory of microtubule catastrophes and their regulation</b>\n
- Brun L, Rupp B, Ward J, Nedelec F\n
- PNAS 106 (50) 21173-21178; 2009\n
- http://www.pnas.org/content/106/50/21173
- </em>
+ If you use this model, please cite:
+ 
+    ***A theory of microtubule catastrophes and their regulation***
+    Brun L, Rupp B, Ward J, Nedelec F
+    PNAS 106 (50) 21173-21178; 2009
+    http://www.pnas.org/content/106/50/21173
 
  The predicted mean time until catastrophe is approximately
 
-     growing_rate = growing_speed / unit_length
-     real ctime = growing_rate / ( 3 * hydrolysis_rate * hydrolysis_rate );
+    growing_rate = growing_speed / unit_length
+    real ctime = growing_rate / ( 3 * hydrolysis_rate * hydrolysis_rate );
  
- The implemented model includes off-rate in the assembly state, as described in:\n
- <em>
- <b>Random Hydrolysis Controls the Dynamic Instability of Microtubules</b>\n
- Ranjith Padinhateeri, Anatoly B Kolomeisky, and David Lacoste\n
- Biophys J 102, 1274–1283 (2012)\n
- http://dx.doi.org/10.1016/j.bpj.2011.12.059
- </em>
+ The implemented model includes off-rate in the assembly state, as described in:
  
+    ***Random Hydrolysis Controls the Dynamic Instability of Microtubules***
+    Ranjith Padinhateeri, Anatoly B Kolomeisky, and David Lacoste
+    Biophys J 102, 1274–1283 (2012)
+    http://dx.doi.org/10.1016/j.bpj.2011.12.059
  
-
- This is not implemented:
- - the MINUS_END is not dynamic,
- - rescues are not included.
+ Moreover:
+ 
+ - Gillespie timers are used for the stochastic model
+ - the MINUS_END can be static or shrinking (parameter `shrinking_rate[1]`)
+ - a simple rescue mechanism was implented as unhydrolyzed_prob[] (Maud Formanek)
  .
  
  See the @ref DynamicFiberPar.
 
- // @todo DynamicFiber detach_rate should depend on the state of the subunit
- // @todo DynamicFiber should keep the entire state vector of the subunits
+ @todo DynamicFiber detach_rate could depend on the state of the subunit
+ @todo DynamicFiber could keep the entire state vector of the subunits
 
- Note: A Gillespie simulation method is used.
- This class is not fully tested (17. Feb 2011).
+ Note:
+ This class is not fully tested
  @ingroup FiberGroup
  */
 class DynamicFiber : public Fiber

src/sim/mecable.cc
@@ -245,19 +245,19 @@ void Mecable::setPoints(const real pts[], const size_t nbp)
 
 /**
 Copy the coordinates of the points of Object to array `ptr[]`, previously
-allocated to hold `ptr_n` coordinates. Thus in 3D, `ptr_n` should be >= 3*Object::nbPoints()
+allocated to hold `cnt` coordinates. Thus in 3D, `cnt` should be >= 3*Object::nbPoints()
 Exactly 3*Object::nbPoints() values are set at most. The Z component is set to zero in 2D mode.
 
 @return error code: 0 = no error, 1 = insufficient allocation
 */
-int Mecable::putPoints(float ptr[], size_t ptr_n) const
+int Mecable::putPoints(float ptr[], size_t cnt) const
 {
 #if ( DIM == 2 )
-    size_t sup = std::min(DIM*nPoints, ptr_n);
+    size_t sup = std::min(DIM*(size_t)nPoints, cnt);
     for ( size_t i = 0; i < sup; ++sup )
         ptr[i] = pPos[i];
 #else
-    size_t sup = std::min(nPoints, ptr_n/3);
+    size_t sup = std::min((size_t)nPoints, cnt/3);
     for ( size_t i = 0; i < sup; ++sup )
     {
         for ( size_t d = 0; d < DIM; ++d )
@@ -266,7 +266,7 @@ int Mecable::putPoints(float ptr[], size_t ptr_n) const
             ptr[3*i+d] = 0;
     }
 #endif
-    return ( ptr_n < 3*nPoints );
+    return ( cnt < 3*nPoints );
 }
 
 

src/sim/mecafil.h
@@ -43,17 +43,17 @@ class Mecafil : public Chain
 private:
     
     /// normalized differences of successive vertices
-    real   *    iDir;
+    real * iDir;
 
     /// Lagrange multipliers associated with longitudinal imcompressibility
-    real   *    iLag;
+    real * iLag;
     
     /// work array allocated to hold `DIM*nbPoints` scalar values
-    real   *    iLLG;
+    real * iLLG;
     
 #if NEW_ANISOTROPIC_FIBER_DRAG
     /// local filament direction vectors used to calculate anisotropic drag
-    real   *    iAni;
+    real * iAni;
 #endif
 
 #if PROJECT_WITH_MATRIX
@@ -61,57 +61,57 @@ private:
     /* variables used for projecting with a matrix ( mecafil_projectmat.cc ) */
     
     /// projection matrix
-    real   *    iProj;
+    real * iProj;
     
     /// differential of projection matrix
-    real   *    iDProj;
+    real * iDProj;
     
     /// part of the projection matrix
-    real   *    iJJtiJ;
+    real * iJJtiJ;
     
     /// intermediate of calculus
-    real   *    iTMP;
+    real * iTMP;
 
 #else
     
     /// J*J' is a tridiagonal symmetric matrix of size (nbPoints-1).
     /** iJJt[] holds the diagonal elements and iJJtU[] the off-diagonal ones. */
-    real   *    iJJt, * iJJtU;
+    real * iJJt, * iJJtU;
 
 #endif
     
     /// vector for the projection correction of size nbSegments
-    real   *    iJJtiJforce;
+    real * iJJtiJforce;
     
     /// true if all elements of iJJtiJforce[] are null
-    bool        useProjectionDiff;
+    bool  useProjectionDiff;
     
 protected:
     
     /// mobility of the points (all points have the same drag coefficient)
-    real        iPointMobility;
+    real  iPointMobility;
     
     /// rigidity scaling factor used in addRigidity()
-    real        iRigidity;
+    real  iRigidity;
     
 #if NEW_FIBER_LOOP
     /// link filament into a loop
-    bool        iRigidityLoop;
+    bool  iRigidityLoop;
 #endif
     
     /// calculate the normalized difference of successive vertices in iDir[]
-    void        storeDirections();
+    void  storeDirections();
 
 private:
     
     /// reset the memory pointers for the projection
-    void        buildProjection();
+    void  buildProjection();
     
     /// allocate memory for the projection
-    void        allocateProjection(size_t);
+    void  allocateProjection(size_t);
     
     /// free the memory for the projection
-    void        destroyProjection();
+    void  destroyProjection();
 
 public:
     

commit 9d8800904a99982f5bb66fbc68437146988db177

fixed bug introduced in 3c2809a5ab739a39c2b370bd787527d5b2df8a60

src/sim/mecable.h
@@ -43,7 +43,7 @@ typedef unsigned short SIZE_T;
 class Mecable : public Object, public Buddy
 {
 protected:
-    
+
     /// array of size DIM*pAllocated contains DIM*nPoints coordinates
     /**
      The coordinates are arranged as follows:
@@ -53,29 +53,36 @@ protected:
      */
     real *      pPos;
 
-    /// Array containing force-coordinates which is allocated in Meca
-    real const* pForce;
-    
-    /// Number of points in pForce[]
-    SIZE_T      pForceMax;
-    
-    /// Number of points in the Mecable
-    SIZE_T      nPoints;
-
 private:
     
-    /// Index that Object coordinates occupy in the matrices and vectors of Meca
-    SIZE_T      pIndex;
-
-    /// Currently allocated size of arrays pPos[]
-    SIZE_T      pAllocated;
+    /// Array containing force-coordinates which is allocated in Meca
+    real const* pForce;
 
     /// Matrix block used for preconditionning in Meca::solve()
     real *      pBlock;
     
     /// Pivot indices for LAPACK
-    int *       pPivot;
+    int  *      pPivot;
     
+    /// Index that Object coordinates occupy in the matrices and vectors of Meca
+    size_t      pIndex;
+
+protected:
+
+    /// Number of points in the Mecable
+    SIZE_T      nPoints;
+
+private:
+
+    /// Number of points in pForce[]
+    SIZE_T      pForceMax;
+
+    /// Currently allocated size of arrays pPos[]
+    SIZE_T      pAllocated;
+    
+    /// type of block
+    SIZE_T      pBlockType;
+
     /// Allocated size of pBlock[]
     SIZE_T      pBlockAlc;
     
@@ -84,13 +91,10 @@ private:
     
     /// Current size of pBlock[]
     SIZE_T      pBlockSize;
-    
-    /// type of block
-    int         pBlockType;
 
 #if EXPERIMENTAL_PRECONDITIONNERS
     /// Flag that pBlock[] is used for preconditionning
-    int         pBlockAge;
+    SIZE_T      pBlockAge;
     
     /// Matrix used for preconditionning
     MatrixFull  pBlockMatrix;
@@ -113,76 +117,76 @@ public:
     //--------------------------------------------------------------------------
     
     /// Set the number of points of the object
-    void            setNbPoints(const size_t n) { allocateMecable(n); nPoints = n; }
+    void         setNbPoints(const size_t n) { allocateMecable(n); nPoints = n; }
     
     /// Returns number of points
-    size_t          nbPoints()     const { return nPoints; }
+    size_t       nbPoints()  const { return nPoints; }
     
     /// Index of the last point = nbPoints() - 1
-    size_t          lastPoint()    const { return nPoints - 1; }
+    size_t       lastPoint() const { return nPoints - 1; }
     
     /// size currently allocated
-    size_t          allocated()    const { return pAllocated; }
+    size_t       allocated() const { return pAllocated; }
 
     //--------------------------------------------------------------------------
     
     /// Position of point 'p' of the object
     /** this is identical to posPoint(), it exists for historical reasons*/
-    const Vector    posP(size_t p)      const { return Vector(pPos+DIM*p); }
+    const Vector posP(size_t p)      const { return Vector(pPos+DIM*p); }
     
     /// Position of vertex number 'p' (indices starting at zero)
-    Vector          posPoint(size_t p)  const { assert_true(pPos && p<nPoints); return Vector(pPos+DIM*p); }
+    Vector       posPoint(size_t p)  const { assert_true(pPos && p<nPoints); return Vector(pPos+DIM*p); }
     
     /// Address of coordinate array
-    const real*     addrPoints()        const { return pPos; }
+    const real * addrPoints()        const { return pPos; }
     
     /// Address of point `p`
-    const real*     addrPoint(size_t p) const { return pPos + DIM*p; }
+    const real * addrPoint(size_t p) const { return pPos + DIM*p; }
 
     /// Set position of point `i` to `x`
-    void            setPoint(size_t i, Vector const& x) { assert_true(i<nPoints); x.store(pPos+DIM*i); }
+    void         setPoint(size_t i, Vector const& x) { assert_true(i<nPoints); x.store(pPos+DIM*i); }
     
     /// Shift point at index `i` by `x`
-    void            movePoint(size_t i, Vector const& x) { assert_true(i<nPoints); x.add_to(pPos+DIM*i); }
+    void         movePoint(size_t i, Vector const& x) { assert_true(i<nPoints); x.add_to(pPos+DIM*i); }
     
     /// replace current coordinates by values from the given array
-    virtual void    getPoints(real const*);
+    virtual void getPoints(real const*);
     
     /// Set to `n_pts` points copied from `pts[]`
-    void            setPoints(const real pts[], size_t n_pts);
+    void         setPoints(const real pts[], size_t n_pts);
     
     /// copy current vertex coordinates to given array, assuming it is suitably allocated
-    void            putPoints(real*) const;
+    void         putPoints(real*) const;
 
     /// Copy current vertex coordinates to `ptr[]`, already allocated to hold `cnt` scalars
-    int             putPoints(float ptr[], size_t cnt) const;
+    int          putPoints(float ptr[], size_t cnt) const;
 
     /// Add a point and expand the object, returning the array index that was used
-    size_t          addPoint(Vector const& w);
+    size_t       addPoint(Vector const& w);
     
     /// Remove `nbp` points starting from index `inx`
-    void            removePoints(size_t inx, size_t nbp);
+    void         removePoints(size_t inx, size_t nbp);
     
     /// Remove all points
-    void            clearPoints()  { nPoints = 0; }
+    void         clearPoints()  { nPoints = 0; }
     
     /// Shift `nbp` points starting from index `inx`
-    void            shiftPoints(size_t inx, size_t nbp);
+    void         shiftPoints(size_t inx, size_t nbp);
     
     /// Remove all points with indices [ 0, p-1 ], keep [ p, nbPoints() ]
-    virtual void    truncateM(size_t p);
+    virtual void truncateM(size_t p);
     
     /// Keep points [ 0, p ], remove other points
-    virtual void    truncateP(size_t p);
+    virtual void truncateP(size_t p);
     
     /// Set all coordinates to zero (nicer for debug/testing)
-    void            resetPoints();
+    void         resetPoints();
     
     /// Add random noise uniformly to all coordinate (used for testing purposes)
-    void            addNoise(real amount);
+    void         addNoise(real amount);
     
     /// calculate first and second momentum of point coordinates
-    void            calculateMomentum(Vector& avg, Vector& dev);
+    void         calculateMomentum(Vector& avg, Vector& dev);
     
     //--------------------------------------------------------------------------
     
@@ -274,7 +278,7 @@ public:
     void        blockSize(size_t, size_t block, size_t pivot);
     
     /// True if preconditionner block is 'in use'
-    int         blockType()  const { return pBlockType; }
+    SIZE_T      blockType()  const { return pBlockType; }
 
     /// Returns address of memory allocated for preconditionning
     real *      block()      const { return pBlock; }
@@ -284,13 +288,13 @@ public:
     
 #if EXPERIMENTAL_PRECONDITIONNERS
     /// Type of block: 0=identity; 1=full; 2=band; 3=custom
-    void        blockType(int t)   { pBlockType = t; pBlockAge = 0; }
+    void        blockType(SIZE_T t) { pBlockType = t; pBlockAge = 0; }
 
     /// True if preconditionner block is 'in use'
-    int         blockAge()   const { return pBlockAge; }
+    SIZE_T      blockAge()   const { return pBlockAge; }
     
     /// Change preconditionning flag
-    void        blockAge(int b)    { pBlockAge = b; }
+    void        blockAge(SIZE_T b) { pBlockAge = b; }
 
     /// Returns matrix allocated for preconditionning
     MatrixFull& blockMatrix()      { return pBlockMatrix; }
@@ -299,7 +303,7 @@ public:
     void        blockMultiply(const real* X, real* Y) const { return pBlockMatrix.vecMul(X, Y); }
 #else
     /// Type of block: 0=identity; 1=full; 2=band; 3=custom
-    void        blockType(int t)   { pBlockType = t; }
+    void        blockType(SIZE_T t) { pBlockType = t; }
 #endif
     //--------------------------------------------------------------------------
     