86f535746430d211535190a465fd0d49d47532eb
francois.nedelec@slcu.cam.ac.uk <francois.nedelec@slcu.cam.ac.uk>
2020-12-08 23:49:04 +0100

    cleaning Meca1D

--- src/sim/meca1d.h
+++ src/sim/meca1d.h
@@ -25,23 +25,21 @@
 class Meca1D
 {
     size_t allocated_;           ///< allocated size of vectors
-    
-    int    ready_;               ///< true if the solution is contained in 'vSOL'
-    
-public:
-   
-    Array<Mecable *> mecables;   ///< list of mobile objects
 
     real * vSOL;                 ///< position of the points
     real * vBAS;                 ///< base points of forces and intermediate of calculus
     real * vMOB;                 ///< the mobility coefficients of the objects
     real * vRHS;                 ///< right-hand side term of the equation
-
-    /// matrix containing the elasticity coefficients
-    SparMatSym1   mA;
     
-    /// working memory allocator
-    LinearSolvers::Allocator allocator;
+    Array<Mecable *> mecables;   ///< list of mobile objects
+
+    SparMatSym1 mat;             ///< matrix containing the elasticity coefficients
+
+    LinearSolvers::Allocator allocator;  ///< working memory allocator
+    
+    int ready_;                  ///< true if the solution is contained in 'vSOL'
+
+public:
 
     Meca1D()
     {
@@ -72,6 +70,22 @@ public:
         vRHS = nullptr;
     }
 
+    void allocate(size_t dim)
+    {
+        if ( dim > allocated_ )
+        {
+            free_real(vBAS);
+            free_real(vSOL);
+            free_real(vMOB);
+            free_real(vRHS);
+            allocated_ = chunk_real(dim);
+            vBAS = new_real(allocated_);
+            vSOL = new_real(allocated_);
+            vMOB = new_real(allocated_);
+            vRHS = new_real(allocated_);
+        }
+    }
+    
     void prepare(Simul const* sim, real time_step, real kT)
     {
         ready_ = 0;
@@ -81,65 +95,51 @@ public:
             mecables.push_back(fib);
 
         size_t dim = mecables.size();
-        if ( dim > allocated_ )
-        {
-            // make a multiple of chunk to align memory:
-            allocated_ = chunk_real(dim);
-            
-            free_real(vBAS);
-            free_real(vSOL);
-            free_real(vMOB);
-            free_real(vRHS);
-            
-            vBAS = new_real(allocated_);
-            vSOL = new_real(allocated_);
-            vMOB = new_real(allocated_);
-            vRHS = new_real(allocated_);
-        }
         
-        mA.resize(dim);
-        mA.reset();
+        allocate(dim);
+        mat.resize(dim);
+        mat.reset();
 
         zero_real(dim, vBAS);
         zero_real(dim, vRHS);
 
-        size_t ii = 0;
+        size_t inx = 0;
         for ( Mecable * mec : mecables )
         {
-            mec->setIndex(ii);
+            mec->setIndex(inx);
             mec->setDragCoefficient();
-            // Put the x coordinate of the origin in vSOL[ii]
-            vSOL[ii] = mec->posPoint(0).XX;
-            vMOB[ii] = time_step / mec->dragCoefficient();
-            ++ii;
+            // Put the x coordinate of the origin in vSOL[inx]
+            vSOL[inx] = mec->posPoint(0).XX;
+            vMOB[inx] = time_step / mec->dragCoefficient();
+            ++inx;
         }
     }
     
-    /// add a clamp between point at index 'ii' to position 'dx'
-    void addClamp(size_t ii, real w, real dx)
+    /// clamp point at index 'i' to position 'pos' with weight w
+    void addClamp(size_t i, real w, real pos)
     {
-        mA(ii, ii) -= w;
-        vBAS[ii]   += w * dx;
+        mat(i, i) -= w;
+        vBAS[i] += w * pos;
     }
     
-    /// add a link between points 'ii' and 'jj' with a position shift 'dx'
-    void addLink(size_t ii, size_t jj, real w, real dx)
+    /// link points 'i' and 'j' with a position offset 'delta' and weight w
+    void addLink(size_t i, size_t j, real w, real delta)
     {
-        mA(ii, ii) -= w;
-        mA(ii, jj) += w;
-        mA(jj, jj) -= w;
-        vBAS[ii] += w * dx;
-        vBAS[jj] -= w * dx;
+        mat(i, i) -= w;
+        mat(i, j) += w;
+        mat(j, j) -= w;
+        vBAS[i] += w * delta;
+        vBAS[j] -= w * delta;
     }
     
     /// add Brownian motion and return smallest magnitude
     real setRightHandSide(real kT)
     {
         real res = INFINITY;
-        for ( size_t ii = 0; ii < mecables.size(); ++ii )
+        for ( size_t i = 0; i < mecables.size(); ++i )
         {
-            real b = std::sqrt( 2 * kT * vMOB[ii] );
-            vRHS[ii] = vMOB[ii] * vBAS[ii] + b * RNG.gauss();
+            real b = std::sqrt( 2 * kT * vMOB[i] );
+            vRHS[i] = vMOB[i] * vBAS[i] + b * RNG.gauss();
             if ( b < res )
                 res = b;
         }
@@ -150,23 +150,23 @@ public:
     /**
      Given that the force can be expressed as:
      
-         F(POS) = mA * ( POS + vBAS )
+         F(POS) = MAT * ( POS + vBAS )
      
-     Where mA is a matrix and vBAS is a vector.
+     Where MAT is a matrix and vBAS is a vector.
      This solves the discretized equation:
 
-         ( newPOS - oldPOS ) / time_step = MOB * mA * ( newPOS + vBAS ) + Noise
+         ( newPOS - oldPOS ) / time_step = MOB * MAT * ( newPOS + vBAS ) + Noise
   
      where MOB is a diagonal matrix of mobility coefficients (ie just a vector).
      Importantly, we used 'newPOS' on the right hand side, for implicit integration!
      We define
      
          vMOB = time_step * MOB
-         vRHS = time_step * MOB * mA * ( oldPOS + vBAS ) + time_step * Noise
+         vRHS = time_step * MOB * MAT * ( oldPOS + vBAS ) + time_step * Noise
      
      leading to:
      
-         ( I - vMOB * mA ) ( newPOS - oldPOS ) = vRHS
+         ( I - vMOB * MAT ) ( newPOS - oldPOS ) = vRHS
      
      We solve the linear system to determine:
      
@@ -176,9 +176,9 @@ public:
     size_t solve(real precision)
     {
         assert_true(ready_==0);
-        mA.prepareForMultiply(1);
+        mat.prepareForMultiply(1);
         LinearSolvers::Monitor monitor(dimension(), precision);
-        // solve without preconditionner
+        // solve system using Bi-Conjugate Gradient Stabilized Method:
         LinearSolvers::BCGS(*this, vRHS, vSOL, monitor, allocator);
         ready_ = monitor.converged();
         return monitor.count();
@@ -189,12 +189,12 @@ public:
     {
         if ( ready_ )
         {
-            size_t ii = 0;
+            size_t i = 0;
             for ( Mecable * mec : mecables )
             {
                 // Move the Mecable along the X direction as calculated
-                mec->translate(Vector(vSOL[ii], 0, 0));
-                ++ii;
+                mec->translate(Vector(vSOL[i], 0, 0));
+                ++i;
             }
         }
     }
@@ -202,16 +202,16 @@ public:
     /// Implements the LinearOperator
     size_t dimension() const { return mecables.size(); }
     
-    /// Implements the LinearOperator:  Y <- X - vMOB * mA * X
+    /// Implements the LinearOperator:  Y <- X - vMOB * MAT * X
     void multiply(const real * X, real * Y) const
     {
         assert_true( X != Y  &&  X != vBAS  &&  Y != vBAS );
         
         zero_real(mecables.size(), vBAS);
-        mA.vecMulAdd(X, vBAS);
+        mat.vecMulAdd(X, vBAS);
         
-        for( size_t ii = 0; ii < mecables.size(); ++ii )
-            Y[ii] = X[ii] - vMOB[ii] * vBAS[ii];
+        for( size_t i = 0; i < mecables.size(); ++i )
+            Y[i] = X[i] - vMOB[i] * vBAS[i];
     }
 };
 
