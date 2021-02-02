6b921af121089864f8fdc263895f7c4e46faaded
francois.nedelec@slcu.cam.ac.uk <francois.nedelec@slcu.cam.ac.uk>
2021-02-02 09:45:33 +0100

    color2 is less transparent

--- src/disp/point_disp.cc
+++ src/disp/point_disp.cc
@@ -480,7 +480,7 @@ void PointDisp::read(Glossary& glos)
     
     // set 'color2' as a darker tone of 'color':
     if ( glos.set(color,   "color") )
-        color2 = color.alpha_scaled(0.25);
+        color2 = color.alpha_scaled(0.5);
     glos.set(color2,       "color", 1) || glos.set(color2, "back_color");
     glos.set(coloring,     "coloring");
     
