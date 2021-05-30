a270f184f188e729c88fbd293a2bee999756567b
francois.nedelec@slcu.cam.ac.uk <francois.nedelec@slcu.cam.ac.uk>
2021-05-30 10:29:45 +0200

    Cleanup test_thread

--- src/test/test_thread.cc
+++ src/test/test_thread.cc
@@ -1,25 +1,29 @@
-// Cytosim was created by Francois Nedelec. Copyright 2007-2017 EMBL.
+// Cytosim was created by Francois Nedelec. Copyright 2021 Cambridge University.
 
 #include <cstdlib>
 #include <cstdio>
 #include <pthread.h>
 
 /*
- An example where the execution of a 'slave' thread,
- is controlled from the current thread (through keyboard input)
+ This is an elementary test where the execution of child threads is controlled
+ from the their parent thread, using a condition signal.
+ Using the C POSIX thread library
  */
 
 pthread_mutex_t mutex;
 pthread_cond_t condition;
 
+thread_local void* thread_id = 0;
+
 
 void* loop(void *arg)
 {
+    thread_id = arg;
     pthread_mutex_lock(&mutex);
     pthread_t self = pthread_self();
     for ( int cnt = 1; cnt <= 10; ++cnt )
     {
-        printf("- thread %lx: step %i", self, cnt);
+        fprintf(stderr, "-- thread %p @ %i -- ", thread_id, cnt);
         pthread_cond_wait(&condition, &mutex);
     }
     pthread_mutex_unlock(&mutex);
@@ -29,20 +33,22 @@ void* loop(void *arg)
 
 int main(int argc, char *argv[]) 
 {
-    printf("test_thread:\n");
-    printf("type `return` to signal to thread or enter `q` to quit\n");
-
+    fprintf(stderr, "type `return` to signa and `q` to quit\n");
+    
     pthread_mutex_init(&mutex, 0);
     pthread_cond_init(&condition, 0);
     
-    pthread_t slave;
-    pthread_create(&slave, 0, &loop, 0);
-    
+    pthread_t slave, child;
+    pthread_create(&slave, 0, &loop, (void*)1);
+    pthread_create(&child, 0, &loop, (void*)2);
+
     char key[32];
-    do {
-        fgets(key, sizeof(key), stdin);
-        pthread_cond_signal(&condition);
-    } while( key[0] != 'q' );
+    while ( fgets(key, sizeof(key), stdin) )
+    {
+        if ( key[0] == 'q' ) break;
+        if ( key[0] == '\n' )
+            pthread_cond_signal(&condition);
+    }
     
     if ( 1 )
     {
@@ -52,5 +58,5 @@ int main(int argc, char *argv[])
     
     pthread_cond_destroy(&condition);
     pthread_mutex_destroy(&mutex);
-    printf("finished\n");
+    fprintf(stderr, "goodbye!\n");
 }
