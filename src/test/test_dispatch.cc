1d4744f03b7452d4af8176c2efca18f6aeca9f43
Francois Nedelec <fjn28@cam.ac.uk>
2021-11-23 11:11:39 +0100

    updated test_dispatch

--- src/test/test_dispatch.cc
+++ src/test/test_dispatch.cc
@@ -1,32 +1,46 @@
 // Cytosim was created by Francois Nedelec. Copyright 2021 Cambridge University
-// [Grand Central Dispatch](https://developer.apple.com/documentation/DISPATCH)
+// [Grand Central Dispatch](https://developer.apple.com/documentation/dispatch)
+// [doc](https://developer.apple.com/documentation/dispatch)
+
 
 #include <dispatch/dispatch.h>
 #include <stdio.h>
 #include <cmath>
+#include <sys/time.h>
 
 
-const size_t CNT = 1024;
+const size_t CNT = 128;
 
 double results[CNT];
 
-void work(size_t i)
+void work(void* context, size_t i)
 {
-    results[i] = std::sin(i);
+    fprintf(stderr, "%lX ", i&15);
+    results[i] = i * i;
+    usleep((1+(i&3))*200000);
+}
+
+void hello(void * context)
+{
+    usleep(100000);
+    const char * str = dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL);
+    printf("Hello, from `%s'!\n", str);
 }
 
 
 int main(int argc, char * argv[])
 {
-    //dispatch_queue_t queue = dispatch_queue_create(nullptr, nullptr)
-    dispatch_queue_t const& queue = dispatch_get_global_queue(0, 0);
+    time_t sec = time(nullptr);
     
-    dispatch_apply(CNT, queue, ^(size_t i){ work(i); });
+    dispatch_queue_t queue = dispatch_queue_create("QUEUE", DISPATCH_QUEUE_CONCURRENT);
     
-    dispatch_sync(queue, ^{ printf("Hello, from a dispatch queue!\n"); });
-
-    //dispatch_release(queue);
+    dispatch_async_f(queue, nullptr, hello);
+    dispatch_apply_f(CNT, queue, nullptr, work);
+    dispatch_release(queue);
+    
     for ( int i = 0; i < 10; ++i )
-        printf("%i %9.3f\n", i, results[i]);
+        printf("\n%i %9.3f", i, results[i]);
+    
+    printf("\n%lu seconds!\n", time(nullptr)-sec);
 }
 
