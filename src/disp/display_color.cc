2f130563a70e8fd9dd0e979286fd3f5d2c2a721a
Francois Nedelec <fjn28@cam.ac.uk>
2022-10-10 21:17:39 +0100

    using anonymous namespace instead of LOCAL

--- src/disp/display_color.cc
+++ src/disp/display_color.cc
@@ -1,244 +1,243 @@
 // Cytosim was created by Francois Nedelec. Copyright 2020 Cambridge University
 
-// restrict functions to local scope
-#define LOCAL static inline
-
-
-// colors that vary with the direction of a vector:
-LOCAL gym_color radial_color(const Vector3& d) { return gym_color::radial_color((float)d.XX, (float)d.YY, (float)d.ZZ, 1.0f); }
-LOCAL gym_color radial_color(const Vector2& d) { return gym_color::radial_colorXY((float)d.XX, (float)d.YY, 1.0f); }
-LOCAL gym_color radial_color(const Vector1& d) { if ( d.XX > 0 ) return gym_color(1,1,1); else return gym_color(0,1,0); }
-
-//------------------------------------------------------------------------------
+// protect local definitions into anonymous namespace
+namespace {
+    
+    // colors that vary with the direction of a vector:
+    gym_color radial_color(const Vector3& d) { return gym_color::radial_color((float)d.XX, (float)d.YY, (float)d.ZZ, 1.0f); }
+    gym_color radial_color(const Vector2& d) { return gym_color::radial_colorXY((float)d.XX, (float)d.YY, 1.0f); }
+    gym_color radial_color(const Vector1& d) { if ( d.XX > 0 ) return gym_color(1,1,1); else return gym_color(0,1,0); }
+    
+    //------------------------------------------------------------------------------
 #pragma mark - Color schemes to draw Fibers
-
-LOCAL real color_scale(Fiber const* fib, int style)
-{
-    switch ( style )
+    
+    real color_scale(Fiber const* fib, int style)
     {
-        case 1: return 1; // color_by_fiber
-        case 2: // color_by_tension
-        case 3: return 1 / fib->prop->disp->tension_scale;
-        case 4: return fib->prop->disp->length_scale; // color_by_curvature
-        case 5: return 1; // color_by_direction
-        case 6: // 6 and 7: color_by_distanceP
-        case 7: return fib->segmentation() / fib->prop->disp->length_scale;
-        case 8: return 1 / fib->prop->disp->length_scale; // color_by_height
-        case 9: return 1; // color_by_grid
+        switch ( style )
+        {
+            case 1: return 1; // color_by_fiber
+            case 2: // color_by_tension
+            case 3: return 1 / fib->prop->disp->tension_scale;
+            case 4: return fib->prop->disp->length_scale; // color_by_curvature
+            case 5: return 1; // color_by_direction
+            case 6: // 6 and 7: color_by_distanceP
+            case 7: return fib->segmentation() / fib->prop->disp->length_scale;
+            case 8: return 1 / fib->prop->disp->length_scale; // color_by_height
+            case 9: return 1; // color_by_grid
+        }
+        return 1;
     }
-    return 1;
-}
-
-LOCAL gym_color color_fiber(Fiber const& fib, size_t)
-{
-    return fib.disp->color;
-}
-
-LOCAL gym_color color_alternate(Fiber const& fib, size_t seg)
-{
-    if ( seg & 1 )
+    
+    gym_color color_fiber(Fiber const& fib, size_t)
+    {
         return fib.disp->color;
-    else
-        return fib.disp->color.darken(0.75);
-}
-
-LOCAL gym_color color_by_tension(Fiber const& fib, size_t seg)
-{
-    real x = fib.disp->color_scale * fib.tension(seg);
-    return fib.disp->color.alpha(x);
-}
-
-LOCAL gym_color color_by_tension_jet(Fiber const& fib, size_t seg)
-{
-    real x = fib.disp->color_scale * fib.tension(seg);
-    // use jet coloring, where Lagrange multipliers are negative under compression
-    return gym_color::jet_color_alpha(x);
-    //return fib.disp->color.alpha(x-1);
-}
-
-LOCAL gym_color color_by_curvature(Fiber const& fib, size_t pti)
-{
-    const real beta = fib.disp->color_scale;
-    return gym_color::jet_color(beta*fib.curvature(pti));
-}
-
-LOCAL gym_color color_seg_curvature(Fiber const& fib, size_t seg)
-{
-    const real beta = fib.disp->color_scale;
-    real c = 0.5 * (fib.curvature(seg) + fib.curvature(seg+1));
-    return gym_color::jet_color(beta*c);
-}
-
-LOCAL gym_color color_by_direction(Fiber const& fib, size_t seg)
-{
-    return radial_color(fib.dirSegment(seg));
-}
-
-/// using distance from the minus end to the start of segment `seg`
-LOCAL gym_color color_by_distanceM(Fiber const& fib, real pti)
-{
-    const real beta = fib.disp->color_scale;
-    real x = std::min(pti*beta, (real)32.0);
-    return fib.disp->color.alpha(std::exp(-x));
-}
-
-/// using the distance from the plus end to vertex `pti`
-LOCAL gym_color color_by_distanceP(Fiber const& fib, real pti)
-{
-    const real beta = fib.disp->color_scale;
-    // using the distance at the vertex
-    real x = std::min((fib.lastPoint()-pti)*beta, (real)32.0);
-    return fib.disp->color.alpha(std::exp(-x));
-}
-
-/// using distance from the plus end to the end of segment `seg`
-LOCAL gym_color color_by_abscissaM(Fiber const& fib, size_t seg)
-{
-    const real beta = fib.disp->color_scale;
-    real x = std::min(seg*beta, (real)32.0);
-    return fib.disp->color.alpha(std::exp(-x));
-}
-
-/// using distance from the plus end to the end of segment `seg`
-LOCAL gym_color color_by_abscissaP(Fiber const& fib, size_t seg)
-{
-    const real beta = fib.disp->color_scale;
-    real x = std::min((fib.lastSegment()-seg)*beta, (real)32.0);
-    return fib.disp->color.alpha(std::exp(-x));
-}
-
-/// color set according to distance to the confining Space
-LOCAL gym_color color_by_height(Fiber const& fib, size_t pti)
-{
-    const real beta = fib.disp->color_scale;
-    real Z = 0;
-    Space const* spc = fib.prop->confine_space_ptr;
-    if ( spc && fib.prop->confine )
-        Z = -spc->signedDistanceToEdge(fib.posPoint(pti));
+    }
+    
+    gym_color color_alternate(Fiber const& fib, size_t seg)
+    {
+        if ( seg & 1 )
+            return fib.disp->color;
+        else
+            return fib.disp->color.darken(0.75);
+    }
+    
+    gym_color color_by_tension(Fiber const& fib, size_t seg)
+    {
+        real x = fib.disp->color_scale * fib.tension(seg);
+        return fib.disp->color.alpha(x);
+    }
+    
+    gym_color color_by_tension_jet(Fiber const& fib, size_t seg)
+    {
+        real x = fib.disp->color_scale * fib.tension(seg);
+        // use jet coloring, where Lagrange multipliers are negative under compression
+        return gym_color::jet_color_alpha(x);
+        //return fib.disp->color.alpha(x-1);
+    }
+    
+    gym_color color_by_curvature(Fiber const& fib, size_t pti)
+    {
+        const real beta = fib.disp->color_scale;
+        return gym_color::jet_color(beta*fib.curvature(pti));
+    }
+    
+    gym_color color_seg_curvature(Fiber const& fib, size_t seg)
+    {
+        const real beta = fib.disp->color_scale;
+        real c = 0.5 * (fib.curvature(seg) + fib.curvature(seg+1));
+        return gym_color::jet_color(beta*c);
+    }
+    
+    gym_color color_by_direction(Fiber const& fib, size_t seg)
+    {
+        return radial_color(fib.dirSegment(seg));
+    }
+    
+    /// using distance from the minus end to the start of segment `seg`
+    gym_color color_by_distanceM(Fiber const& fib, real pti)
+    {
+        const real beta = fib.disp->color_scale;
+        real x = std::min(pti*beta, (real)32.0);
+        return fib.disp->color.alpha(std::exp(-x));
+    }
+    
+    /// using the distance from the plus end to vertex `pti`
+    gym_color color_by_distanceP(Fiber const& fib, real pti)
+    {
+        const real beta = fib.disp->color_scale;
+        // using the distance at the vertex
+        real x = std::min((fib.lastPoint()-pti)*beta, (real)32.0);
+        return fib.disp->color.alpha(std::exp(-x));
+    }
+    
+    /// using distance from the plus end to the end of segment `seg`
+    gym_color color_by_abscissaM(Fiber const& fib, size_t seg)
+    {
+        const real beta = fib.disp->color_scale;
+        real x = std::min(seg*beta, (real)32.0);
+        return fib.disp->color.alpha(std::exp(-x));
+    }
+    
+    /// using distance from the plus end to the end of segment `seg`
+    gym_color color_by_abscissaP(Fiber const& fib, size_t seg)
+    {
+        const real beta = fib.disp->color_scale;
+        real x = std::min((fib.lastSegment()-seg)*beta, (real)32.0);
+        return fib.disp->color.alpha(std::exp(-x));
+    }
+    
+    /// color set according to distance to the confining Space
+    gym_color color_by_height(Fiber const& fib, size_t pti)
+    {
+        const real beta = fib.disp->color_scale;
+        real Z = 0;
+        Space const* spc = fib.prop->confine_space_ptr;
+        if ( spc && fib.prop->confine )
+            Z = -spc->signedDistanceToEdge(fib.posPoint(pti));
 #if ( DIM > 2 )
-    else
-        Z = fib.posPoint(pti).ZZ;
+        else
+            Z = fib.posPoint(pti).ZZ;
 #endif
-    return gym_color::jet_color(Z*beta);
-}
-
-
-/// color set according to some Map
-LOCAL gym_color color_by_grid(Fiber const& fib, size_t seg)
-{
-    Map<DIM> const& map = fib.simul().visibleMap();
-    Vector w = fib.midPoint(seg, 0.5);
-    size_t i = map.index(w);
-    return gym::alt_color(i);
-}
-
-
-//------------------------------------------------------------------------------
+        return gym_color::jet_color(Z*beta);
+    }
+    
+    
+    /// color set according to some Map
+    gym_color color_by_grid(Fiber const& fib, size_t seg)
+    {
+        Map<DIM> const& map = fib.simul().visibleMap();
+        Vector w = fib.midPoint(seg, 0.5);
+        size_t i = map.index(w);
+        return gym::alt_color(i);
+    }
+    
+    
+    //------------------------------------------------------------------------------
 #pragma mark - Color schemes for Fiber Lattice
-
-LOCAL gym_color color_alternate(Fiber const& fib, long ix, real)
-{
-    if ( ix & 1 )
-        return fib.disp->color;
-    else
-        return fib.disp->color.darken(0.75);
-}
-
-LOCAL gym_color color_by_lattice(Fiber const& fib, long ix, real beta)
-{
-    return fib.disp->color.darken(beta*fib.visibleLattice()->data(ix));
-}
-
-LOCAL gym_color color_by_lattice_jet(Fiber const& fib, long ix, real beta)
-{
-    return gym_color::jet_color(beta*fib.visibleLattice()->data(ix));
-}
-
-LOCAL gym_color color_by_lattice_white(Fiber const& fib, long ix, real beta)
-{
-    real x = beta * fib.visibleLattice()->data(ix);
-    return gym_color(x, x, x);
-}
-
-LOCAL gym_color lattice_color(gym_color const& col, real val)
-{
-    return col.darken(val);
-    //return gym_color::jet_color(val);
-}
-
-//------------------------------------------------------------------------------
+    
+    gym_color color_alternate(Fiber const& fib, long ix, real)
+    {
+        if ( ix & 1 )
+            return fib.disp->color;
+        else
+            return fib.disp->color.darken(0.75);
+    }
+    
+    gym_color color_by_lattice(Fiber const& fib, long ix, real beta)
+    {
+        return fib.disp->color.darken(beta*fib.visibleLattice()->data(ix));
+    }
+    
+    gym_color color_by_lattice_jet(Fiber const& fib, long ix, real beta)
+    {
+        return gym_color::jet_color(beta*fib.visibleLattice()->data(ix));
+    }
+    
+    gym_color color_by_lattice_white(Fiber const& fib, long ix, real beta)
+    {
+        real x = beta * fib.visibleLattice()->data(ix);
+        return gym_color(x, x, x);
+    }
+    
+    gym_color lattice_color(gym_color const& col, real val)
+    {
+        return col.darken(val);
+        //return gym_color::jet_color(val);
+    }
+    
+    //------------------------------------------------------------------------------
 #pragma mark - Color schemes for Sphere / Solid / Bead
-
-/**
- returns the front color used to display Object
- if `coloring` is enabled, this loads the N-th bright color,
- otherwise load the object's display color
- */
-template < typename T >
-gym_color bodyColorF(T const& obj)
-{
-    const PointDisp * disp = obj.prop->disp;
-    if ( disp->coloring )
+    
+    /**
+     returns the front color used to display Object
+     if `coloring` is enabled, this loads the N-th bright color,
+     otherwise load the object's display color
+     */
+    template < typename T >
+    gym_color bodyColorF(T const& obj)
     {
-        size_t i = ( disp->coloring == 2 ? obj.mark() : obj.signature());
-        return gym::bright_color(i).match_a(disp->color);
+        const PointDisp * disp = obj.prop->disp;
+        if ( disp->coloring )
+        {
+            size_t i = ( disp->coloring == 2 ? obj.mark() : obj.signature());
+            return gym::bright_color(i).match_a(disp->color);
+        }
+        return disp->color;
     }
-    return disp->color;
+    
+    /**
+     returns the front color used to display Object
+     if `coloring` is enabled, this loads the N-th bright color,
+     otherwise load the object's display color
+     */
+    gym_color bodyColorF(PointDisp const* disp, size_t s)
+    {
+        if ( disp->coloring )
+            return gym::bright_color(s).match_a(disp->color);
+        return disp->color;
+    }
+    
+    /**
+     Sets color material for lighting mode
+     if `coloring` is enabled, this loads a bright color,
+     otherwise load the object's display color
+     */
+    template < typename T >
+    void bodyColor(T const& obj)
+    {
+        const PointDisp * disp = obj.prop->disp;
+        if ( disp->coloring )
+        {
+            size_t i = ( disp->coloring == 2 ? obj.mark() : obj.signature());
+            gym_color col = gym::bright_color(i);
+            gym::color_load(col);
+            gym::color_back(col.darken(0.5));
+        }
+        else
+        {
+            gym::color_front(disp->color, 1.0);
+            gym::color_back(disp->color2);
+        }
+    }
+    
+    /**
+     Sets color material for lighting mode
+     if `coloring` is enabled, this loads the N-th bright color,
+     otherwise load the object's display color
+     */
+    void bodyColor(PointDisp const* disp, size_t s)
+    {
+        if ( disp->coloring )
+        {
+            gym_color col = gym::bright_color(s);
+            gym::color_front(col);
+            gym::color_back(col.darken(0.5));
+        }
+        else
+        {
+            gym::color_front(disp->color, 1.0);
+            gym::color_back(disp->color2);
+        }
+    }
+    
 }
-
-/**
- returns the front color used to display Object
- if `coloring` is enabled, this loads the N-th bright color,
- otherwise load the object's display color
- */
-LOCAL gym_color bodyColorF(PointDisp const* disp, size_t s)
-{
-    if ( disp->coloring )
-        return gym::bright_color(s).match_a(disp->color);
-    return disp->color;
-}
-
-/**
-Sets color material for lighting mode
-if `coloring` is enabled, this loads a bright color,
-otherwise load the object's display color
-*/
-template < typename T >
-void bodyColor(T const& obj)
-{
-    const PointDisp * disp = obj.prop->disp;
-    if ( disp->coloring )
-    {
-        size_t i = ( disp->coloring == 2 ? obj.mark() : obj.signature());
-        gym_color col = gym::bright_color(i);
-        gym::color_load(col);
-        gym::color_back(col.darken(0.5));
-    }
-    else
-    {
-        gym::color_front(disp->color, 1.0);
-        gym::color_back(disp->color2);
-    }
-}
-
-/**
- Sets color material for lighting mode
- if `coloring` is enabled, this loads the N-th bright color,
- otherwise load the object's display color
- */
-LOCAL void bodyColor(PointDisp const* disp, size_t s)
-{
-    if ( disp->coloring )
-    {
-        gym_color col = gym::bright_color(s);
-        gym::color_front(col);
-        gym::color_back(col.darken(0.5));
-    }
-    else
-    {
-        gym::color_front(disp->color, 1.0);
-        gym::color_back(disp->color2);
-    }
-}
-
-#undef LOCAL
