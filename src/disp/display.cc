a5b93225b156370d0487d52443ac0ccca9b44e53
Francois Nedelec <fjn28@cam.ac.uk>
2022-12-12 15:56:37 +0000

    football display only first bead

--- src/disp/display.cc
+++ src/disp/display.cc
@@ -1864,7 +1864,7 @@ void Display::drawSolidT(Solid const& obj, size_t inx) const
     const PointDisp * disp = obj.prop->disp;
 
 #if NEW_SOLID_HAS_TWIN && ( DIM > 2 )
-    if (( disp->style & 2 ) && ( obj.radius(inx) > 0 ))
+    if (( inx == 0 ) && ( obj.radius(inx) > 0 ))
     {
         Vector X = obj.posP(inx);
         gym::enableLighting();
