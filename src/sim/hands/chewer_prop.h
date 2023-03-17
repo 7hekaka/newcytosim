b681b64e034fcb3ea975d12a8a581dde1b75a079
Francois Nedelec <fjn28@cam.ac.uk>
2023-03-17 14:53:19 +0100

    Credits to cambridge

--- src/sim/hands/chewer_prop.h
+++ src/sim/hands/chewer_prop.h
@@ -1,4 +1,4 @@
-// Cytosim was created by Francois Nedelec. Copyright 2007-2017 EMBL.
+// Cytosim was created by Francois Nedelec. Copyright 2023 Cambridge University.
 
 #ifndef CHEWER_PROP_H
 #define CHEWER_PROP_H
