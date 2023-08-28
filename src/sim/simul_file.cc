11633c3e5d941282c494577359d52ff7d30b5024
Francois Nedelec <fjn28@cam.ac.uk>
2023-08-28 17:40:45 +0200

    +unlock()

--- src/sim/simul_file.cc
+++ src/sim/simul_file.cc
@@ -135,8 +135,9 @@ void Simul::writeObjects(std::string const& name, bool append, bool binary) cons
     }
     catch( InvalidIO & e )
     {
+        out.unlock();
         print_blue(std::cerr, e.brief());
-        std::cerr << ", writing trajectory file" << '\n';
+        std::cerr << ", writing trajectory at t=" << time() << '\n';
     }
 }
 
