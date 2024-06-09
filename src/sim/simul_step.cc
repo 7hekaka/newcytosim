857336a8f66337db49cc7b911e87a7db7d5ae12b
Francois Nedelec <fjn28@cam.ac.uk>
2024-06-09 12:43:56 +0200

    Re-ordering only

--- src/sim/simul_step.cc
+++ src/sim/simul_step.cc
@@ -1,4 +1,92 @@
-// Cytosim was created by Francois Nedelec. Copyright 2007-2017 EMBL.
+// Cytosim was created by Francois Nedelec. Copyright 2024 Cambridge University
+
+/**
+ This is the master Monte-Carlo step function.
+ 
+ Lists are mixed such that objects are considered in a different
+ and random order at each step, to avoid biais in the simulation
+
+ steps() is called for every list, calling step() for every Object
+ */
+void Simul::steps()
+{
+    //auto rdt = timer();
+    // increment time:
+    prop.time += prop.time_step;
+    //fprintf(stderr, "\n----------------------------------- time is %8.3f\n", prop.time);
+
+    // Monte-Carlo step for all objects
+    events.steps();
+    organizers.steps();
+    tubules.steps();
+    fields.steps();
+    spaces.steps();
+    spheres.steps();
+    beads.steps();
+    solids.steps();
+
+    //printf("     ::steps    %16llu\n", (timer()-rdt)>>5); rdt = timer();
+
+#if POOL_UNATTACHED > 1
+    doAttachCounter = ( doAttachCounter + 1 ) % POOL_UNATTACHED;
+    if ( doAttachCounter )
+    {
+        couples.stepsSkippingUnattached();
+        singles.stepsSkippingUnattached();
+        //printf("     ::noattach %16llu\n", (timer()-rdt)>>3);
+    }
+    else
+#endif
+    {
+        // distribute Fibers over a grid preparing for binding of Hands:
+        const real range = maxBindingRange();
+        fiberGrid.paintGrid(fibers.first(), nullptr, range);
+        
+        //printf("     ::paint    %16llu\n", (timer()-rdt)>>5); rdt = timer();
+        
+        if ( 0 )
+        {
+            // This code continuously tests the binding algorithm.
+            HandProp hp("test_binding");
+            hp.binding_rate  = INFINITY;
+            hp.binding_range = RNG.preal() * range;
+            hp.bind_also_end = BOTH_ENDS;
+            hp.complete(*this);
+            
+            Space const* spc = spaces.master();
+            for ( size_t i = 0; i < 16; ++i )
+            {
+                Vector pos = spc->place();
+                fiberGrid.testAttach(stdout, pos, fibers, &hp);
+            }
+        }
+
+        // step Hand-containing objects, giving them a possibility to attach Fibers:
+        couples.steps();
+        singles.steps();
+        //printf("     ::attach   %16llu\n", (timer()-rdt)>>3);
+    }
+
+    // This will also update all the attached Hands
+    fibers.steps();
+
+    fresh_ = 1;
+}
+
+
+//------------------------------------------------------------------------------
+#pragma mark -
+
+
+// calculate grid range from Hand's binding range:
+real Simul::maxBindingRange() const
+{
+    real res = 0.0;
+    for ( Property const* i : properties.find_all("hand") )
+        res = std::max(res, static_cast<HandProp const*>(i)->binding_range);
+    return res;
+}
+
 
 /**
  @returns the largest segmentation of all existing FiberProp, multiplied by 0.5
@@ -90,90 +178,6 @@ void Simul::prepare()
     couples.prepare(properties);
     
     primed_ = 2;
-}
-
-
-// calculate grid range from Hand's binding range:
-real Simul::maxBindingRange() const
-{
-    real res = 0.0;
-    for ( Property const* i : properties.find_all("hand") )
-        res = std::max(res, static_cast<HandProp const*>(i)->binding_range);
-    return res;
-}
-
-
-/**
- This is the master Monte-Carlo step function.
- 
- Lists are mixed such that objects are considered in a different
- and random order at each step, to avoid biais in the simulation
-
- steps() is called for every list, calling step() for every Object
- */
-void Simul::steps()
-{
-    //auto rdt = timer();
-    // increment time:
-    prop.time += prop.time_step;
-    //fprintf(stderr, "\n----------------------------------- time is %8.3f\n", prop.time);
-
-    // Monte-Carlo step for all objects
-    events.steps();
-    organizers.steps();
-    tubules.steps();
-    fields.steps();
-    spaces.steps();
-    spheres.steps();
-    beads.steps();
-    solids.steps();
-
-    //printf("     ::steps    %16llu\n", (timer()-rdt)>>5); rdt = timer();
-
-#if POOL_UNATTACHED > 1
-    doAttachCounter = ( doAttachCounter + 1 ) % POOL_UNATTACHED;
-    if ( doAttachCounter )
-    {
-        couples.stepsSkippingUnattached();
-        singles.stepsSkippingUnattached();
-        //printf("     ::noattach %16llu\n", (timer()-rdt)>>3);
-    }
-    else
-#endif
-    {
-        // distribute Fibers over a grid preparing for binding of Hands:
-        const real range = maxBindingRange();
-        fiberGrid.paintGrid(fibers.first(), nullptr, range);
-        
-        //printf("     ::paint    %16llu\n", (timer()-rdt)>>5); rdt = timer();
-        
-        if ( 0 )
-        {
-            // This code continuously tests the binding algorithm.
-            HandProp hp("test_binding");
-            hp.binding_rate  = INFINITY;
-            hp.binding_range = RNG.preal() * range;
-            hp.bind_also_end = BOTH_ENDS;
-            hp.complete(*this);
-            
-            Space const* spc = spaces.master();
-            for ( size_t i = 0; i < 16; ++i )
-            {
-                Vector pos = spc->place();
-                fiberGrid.testAttach(stdout, pos, fibers, &hp);
-            }
-        }
-
-        // step Hand-containing objects, giving them a possibility to attach Fibers:
-        couples.steps();
-        singles.steps();
-        //printf("     ::attach   %16llu\n", (timer()-rdt)>>3);
-    }
-
-    // This will also update all the attached Hands
-    fibers.steps();
-
-    fresh_ = 1;
 }
 
 
