231db3c9e4e165c1e126eff40c1f243a3b9e6e7f
Francois Nedelec <fjn28@cam.ac.uk>
2024-10-08 17:04:47 +0200

    Understand fiber's style 'striped'

--- src/disp/fiber_disp.cc
+++ src/disp/fiber_disp.cc
@@ -71,7 +71,8 @@ void FiberDisp::clear()
 
 void FiberDisp::read(Glossary& glos)
 {
-    glos.set(style, "style", {{"line", 0}, {"backbone", 1}, {"stripe", 2}, {"filament", 3}, {"actin", 4}, {"microtubule", 5}});
+    glos.set(style, "style", {{"line", 0}, {"backbone", 1}, {"stripe", 2},
+        {"striped", 2}, {"filament", 3}, {"actin", 4}, {"microtubule", 5}});
     glos.set(visible, "visible");
     if ( glos.set(color, "color") )
         back_color = color.darken(0.625);
