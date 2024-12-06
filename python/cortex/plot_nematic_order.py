008e330289be4d22c9356a3ea0ebb2c28e784a3f
Francois Nedelec <fjn28@cam.ac.uk>
2024-12-06 11:12:48 +0100

    +plot_nematic_order.py

---
+++ python/cortex/plot_nematic_order.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+#
+# Plot nematic order, calculated by `report fiber:nematic`
+#
+# F. Nedelec, Strasbourg, 06-07.12.2024
+
+
+"""
+Description:
+    Plot data calculated by `report fiber:nematic verbose=0 > order.txt`
+
+Syntax:
+    plot_nematic_order.py DIRECTORY_PATH
+
+"""
+
+# time cut off (seconds) above which data is averaged
+cutoff = 4000
+
+#font size:
+fts = 14
+
+import sys, os, math, subprocess
+import matplotlib
+import matplotlib.pyplot as plt
+#matplotlib.use('SVG')
+
+
+def uncode(arg):
+    try:
+        return arg.decode('utf-8')
+    except:
+        return arg
+
+
+def retreive_data(file):
+    """
+        Retreive data from file
+    """
+    T = []
+    D = []
+    ts = 0
+    for line in file:
+        v = uncode(line).split()
+        t = 0
+        d = []
+        if len(v) < 2:
+            pass
+        elif v[0] == '%' and v[1] == "time":
+            t = float(v[2])
+        elif len(v) == 7:
+            t = float(v[1])
+            d = float(v[3])
+        if t < ts:
+            sys.stderr.write(f'Warning: discarding {len(T)} duplicated datalines\n')
+            T = []
+            D = []
+            ts = 0
+            continue
+        if d:
+            T.append(t)
+            D.append(d)
+            ts = t
+    return T, D
+
+
+def plot_data(X, Y, M, name):
+    """
+        Plot data as a function of time
+    """
+    Xsup = math.ceil(max(X)/100)*100
+    Ysup = math.ceil(max(Y))
+    fig = plt.figure(figsize=(4, 3))
+    plt.plot(X, Y, label='nematic order', linewidth=4.0)
+    plt.plot([0, Xsup], [M, M], linewidth=1.0)
+    plt.xlim(0, Xsup)
+    plt.ylim(0, Ysup)
+    plt.xlabel('Time (s)', fontsize=fts)
+    plt.ylabel('Order', fontsize=fts)
+    plt.title('Nematic order', fontsize=fts)
+    plt.legend()
+    fig.tight_layout()
+    plt.savefig('order.png', dpi=150)
+    #plt.show()
+    plt.close()
+
+
+def process(dirpath):
+    """
+        Generate a plot in given directory
+    """
+    filename = "order.txt"
+    if not os.path.isfile(filename):
+        if not os.path.isfile("objects.cmo"):
+            sys.stderr.write(f'Warning: missing file {dirpath}/objects.cmo\n')
+            return
+        else:
+            # attempt to generate report file:
+            args = ["report3", "fiber:nematic", "verbose=0"]
+            subprocess.call(args, stdout=open(filename, 'w'))
+    res = 0
+    with open(filename, 'r') as f:
+        T, D = retreive_data(f)
+        avg = math.nan
+        if T:
+            #print(T, D)
+            # calculate mean length for data above cutoff:
+            val = [ x for t,x in zip(T,D) if t > cutoff ]
+            if val:
+                avg = sum(val) / len(val)
+            plot_data(T, D, avg, dirpath)
+        else:
+            sys.stderr.write(f'Warning: no data in {dirpath}/{filename}\n')
+    print(f'{dirpath} {avg:6.3f}')
+
+
+#------------------------------------------------------------------------
+
+def main(args):
+    paths = []
+    for arg in args:
+        if os.path.isdir(arg):
+            paths.append(arg)
+        else:
+            sys.stderr.write(f"Error: unexpected argument `{arg}`\n")
+            sys.exit()
+    if not paths:
+        process('.')
+    else:
+        cdir = os.getcwd()
+        for p in paths:
+            os.chdir(p)
+            process(p)
+            os.chdir(cdir)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1].endswith("help"):
+        print(__doc__)
+    else:
+        main(sys.argv[1:])
+
