63eda149b52e24370778ef9e5d8cab42c40c06ca
Francois Nedelec <fjn28@cam.ac.uk>
2025-05-30 16:29:08 +0200

    can handle empty directory

--- python/cortex/plot_nematic_order.py
+++ python/cortex/plot_nematic_order.py
@@ -2,7 +2,7 @@
 #
 # Plot nematic order, calculated by `report fiber:nematic`
 #
-# F. Nedelec, Strasbourg, Chitry, 06-30.12.2024, 6.02.2025
+# F. Nedelec, Strasbourg, Chitry, 06-30.12.2024, 6.02.2025 30.05.2025
 
 
 """
@@ -14,8 +14,10 @@ Syntax:
 
 """
 
-# time cut off (seconds) above which data is averaged
-cutoff = 4000
+# time (seconds) above which data is averaged
+threshold = 4000
+# cut off for display of data on graphs
+cutoff = 0
 
 # size of marker in plots:
 mks = 3
@@ -77,12 +79,12 @@ def plot_data(X, Y, M = math.nan):
     """
         Plot data as a function of time, if M is specified, add horizontal line
     """
-    Xinf = math.floor(min(X)/100)*100
+    Xinf = 0#math.floor(min(X)/100)*100
     Xsup = 8000#math.ceil(max(X)/100)*100
     Yinf = 0.2
     Ysup = math.ceil(max(Y))
     fig, axes = plt.subplots(figsize=(6, 4.5))
-    axes.plot(X, Y, label='Nematic Order', marker='o', markersize=mks, color=(1,0,1))
+    axes.plot(X, Y, label='Nematic Order', marker='o', markersize=mks, color=(0.5,0,1))
     if not math.isnan(M):
         axes.plot([Xinf, Xsup], [M, M], linewidth=1.0)
     plt.xlim(Xinf, Xsup)
@@ -114,7 +116,7 @@ def process(dirpath):
     if not os.path.isfile(filename):
         if not os.path.isfile("objects.cmo"):
             sys.stderr.write(f'Warning: missing file {dirpath}/objects.cmo\n')
-            return
+            return ([], [])
         else:
             # attempt to generate report file:
             sys.stderr.write(f"plot_nematic_order.py makes {dirpath}/order.txt");
@@ -122,10 +124,10 @@ def process(dirpath):
             subprocess.call(args, stdout=open(filename, 'w'))
     print(f'{dirpath}', end=' ')
     T, D = retreive_data(filename)
-    if T:
+    if T and D:
         #print(T, D)
-        # calculate mean length for data above cutoff:
-        val = [ x for t,x in zip(T,D) if t > cutoff ]
+        # calculate mean length for data above threshold:
+        val = [ x for t,x in zip(T,D) if t > threshold ]
         if val:
             avg = sum(val) / len(val)
         else:
@@ -133,7 +135,9 @@ def process(dirpath):
         plot_data(T, D, avg)
         save_plot(output);
         print(f' {avg:6.3f} {max(T):9.0f}')
-    return (T, D)
+        return (T, D)
+    else:
+        return ([], [])
 
 
 def redish_color():
@@ -157,7 +161,7 @@ def blueish_color():
 #------------------------------------------------------------------------
 
 def main(args):
-    global format, output
+    global format, output, cutoff
     paths = []
     files = []
     for arg in args:
@@ -170,8 +174,12 @@ def main(args):
         elif arg in ('png', 'svg' ):
             format = arg
         else:
-            sys.stderr.write(f"Error: unexpected argument `{arg}`\n")
-            sys.exit()
+            [key, _, val] = arg.partition('=')
+            if key = 'cutoff':
+                cutoff = float(val)
+            else:
+                sys.stderr.write(f"Error: unexpected argument `{arg}`\n")
+                sys.exit()
     if not paths and not files:
         paths = ['.']
     # generate/collect all data:
@@ -180,21 +188,23 @@ def main(args):
     for p in paths:
         os.chdir(p)
         T, D = process(p)
-        if T:
+        if T and D:
             data.append((T,D))
         os.chdir(cdir)
     for f in files:
         T, D = retreive_data(f)
-        if T:
+        if T and D:
             data.append((T,D))
     # make conbined plot:
     if data:
         T, D = data[0]
-        fig, axes = plot_data(T, D)
+        Tx = [ x-cutoff for x in T ];
+        fig, axes = plot_data(Tx, D)
         for T, D in data[1:5]:
-            axes.plot(T, D, marker='o', markersize=mks, color=blueish_color())
+            Tx = [ x-cutoff for x in T ];
+            axes.plot(Tx, D, marker='o', markersize=mks, color=blueish_color())
         for T, D in data[5:]:
-            Tx = [ x-1000 for x in T];
+            Tx = [ x-cutoff for x in T ];
             axes.plot(Tx, D, marker='o', markersize=mks, color=greenish_color())
         save_plot(output);
 
